ba4d5b139108c9eda7ee0d1d48dfba0f67f3e928
Kavrese <57292303+Kavrese@users.noreply.github.com>
2021-01-09 01:01:55 +0300

    update recycler view

--- app/src/main/java/com/example/weatherapp/AdapterWeather.kt
+++ app/src/main/java/com/example/weatherapp/AdapterWeather.kt
@@ -3,11 +3,13 @@ package com.example.weatherapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterWeather(private val list: MutableList<ModelDay>): RecyclerView.Adapter<AdapterWeather.ViewHolder>() {
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val textT = itemView.findViewById<TextView>(R.id.textT)
+        val textDate = itemView.findViewById<TextView>(R.id.textDate)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -16,7 +18,13 @@ class AdapterWeather(private val list: MutableList<ModelDay>): RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var t = list[position].temp!!.day!!.toInt().toString()
 
+        if ("-" !in t)
+            t = "+$t"
+        holder.textT.text = t
+
+        holder.textDate.text = list[position].dt.toString()
     }
 
     override fun getItemCount(): Int = list.size
